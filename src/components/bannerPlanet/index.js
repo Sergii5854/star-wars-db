7ebb0f28539fd851b84cbce022a8024da966cc30
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-12-15 12:47:13 +0200

    add prop types to components

--- src/components/bannerPlanet/index.js
+++ src/components/bannerPlanet/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import SwapiService from '../../services/swapiService/';
 import Spinner from './../spinner/';
@@ -14,13 +15,7 @@ export default class BannerPlanet extends Component {
         updateInterval: 10000
     }
     static propTypes = {
-        updateInterval: (props, propName, componentName)=>{
-            const value = props[propName]
-
-            if( typeof value === 'nunber' && !isNaN(value)) return null
-
-            return new TypeError(`${componentName}: ${propName} - must be number`)
-        }
+        updateInterval: PropTypes.number
     }
 
     constructor() {
--- src/components/row/index.js
+++ src/components/row/index.js
@@ -1,6 +1,7 @@
-import React  from 'react';
+import React from 'react';
+import ProtoTypes from 'prop-types';
 
-const Row = ({left , right}) =>{
+const Row = ({left, right}) => {
     return (
         <div className="row mb2">
             <div className="col-md-6">
@@ -13,6 +14,11 @@ const Row = ({left , right}) =>{
 
 
     )
+}
+
+Row.ProtoTypes = {
+    left: ProtoTypes.node,
+    right: ProtoTypes.node
 }
 
 export default Row
