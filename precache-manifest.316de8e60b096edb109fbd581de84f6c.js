2e0339cf173989cab3e03a26c5e8e6f4fe5599ef
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-12-16 19:26:22 +0200

    Updates

---
+++ precache-manifest.316de8e60b096edb109fbd581de84f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25275995be36cc1c3af1",
+    "url": "/star-wars-db/static/js/runtime~main.25275995.js"
+  },
+  {
+    "revision": "c0e53fedeaab1a15d7b8",
+    "url": "/star-wars-db/static/js/main.c0e53fed.chunk.js"
+  },
+  {
+    "revision": "093c867c96be43ff3b04",
+    "url": "/star-wars-db/static/js/1.093c867c.chunk.js"
+  },
+  {
+    "revision": "c0e53fedeaab1a15d7b8",
+    "url": "/star-wars-db/static/css/main.c36625b1.chunk.css"
+  },
+  {
+    "revision": "550cf7112cd5243fab56ab8b58d73d3e",
+    "url": "/star-wars-db/index.html"
+  }
+];
